542ba65613c59e479066d5d36b092c7bb956d3d5
Dawid <31769799+wybielak@users.noreply.github.com>
2023-04-03 15:25:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function hideMenu()
+        {
+            document.getElementById("menu").style.right="-100%";
+        }
+
+        function showMenu()
+        {
+            document.getElementById("menu").style.right="0";
+        }
